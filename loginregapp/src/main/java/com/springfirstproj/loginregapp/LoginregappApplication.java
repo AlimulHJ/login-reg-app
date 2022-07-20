4f2bc384c7707e22c5d18d83dd40391a0c2daa3e
Alimul Hasan <alimulhasan000@gmail.com>
2022-07-20 23:19:03 +0600

    first commit - After initializing the project

---
+++ loginregapp/src/main/java/com/springfirstproj/loginregapp/LoginregappApplication.java
@@ -0,0 +1,13 @@
+package com.springfirstproj.loginregapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginregappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginregappApplication.class, args);
+	}
+
+}
---
+++ loginregapp/src/test/java/com/springfirstproj/loginregapp/LoginregappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springfirstproj.loginregapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginregappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
